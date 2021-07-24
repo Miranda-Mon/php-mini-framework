46785ebec18b9d4e62c2a39d4ed66813189de4e5
MirandaM0N <nuno_miranda@outlook.com>
2021-07-24 17:58:26 +0100

    estrutura do projeto

---
+++ app/config/config.php
@@ -0,0 +1,4 @@
+<?php
+define("BASE","/mini-framework");
+define("UNSET_URI_COUNT",1);
+define("DEBUG_URI",true);
---
+++ vendor/autoload.php
@@ -0,0 +1,3 @@
+<?php
+require_once('../app/controller/TesteController.php'); // include autoloader function
+require_once('../app/core/RouterCore.php'); // include autoloader function
